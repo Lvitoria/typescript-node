9f1b58d80f4998b10dd04c75793a3cb190be4f0e
Lucas Vitoria Alves <lucas.alves@pampa.compasso>
2020-02-26 15:05:56 -0300

    ajuste no nome

--- src/schemas/User.ts
+++ src/schemas/User.ts
@@ -2,21 +2,21 @@ import {Document, Schema, Model, model} from 'mongoose'
 
 interface UserInterface extends Document{
     email?: string
-    fisrtName?: string
+    firstName?: string
     lastName?: string
     fullName(inicio): string
 }
 
 const UserSchema = new Schema({
     email: String,
-    fisrtName: String,
+    firstName: String,
     lastName: String
 },{
     timestamps: true
 })
 
 UserSchema.methods.fullName = function (inicio): string{
-    return inicio +': '+this.fisrtName +  ' ' + this.lastName
+    return inicio +': '+this.firstName +  ' ' + this.lastName
 }
 
 export default model<UserInterface>('User', UserSchema)
